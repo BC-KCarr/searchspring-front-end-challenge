dbbbab3b8dee3ef72ebf21c2383aa689040325d4
Kevin <kevincarr632@gmail.com>
2021-01-25 16:56:06 -0600

    removed some not needed boilerplate

---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SearchBar({ keyword, setKeyword }) {
+  return (
+    <input value={keyword} placeholder="Search by name" onChange={(e) => setKeyword(e.target.value)} className="searchBar"/>
+  )
+}
+
+export default SearchBar
