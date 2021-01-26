7c1ac7e1c36faddd56052f1eabfb4d650aa8348f
Kevin <kevincarr632@gmail.com>
2021-01-25 23:46:14 -0600

    added pagination

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,24 +1,28 @@
 import React, { useState, useCallback, useEffect } from 'react'
 import SearchBar from './SearchBar'
 import ProductsList from './ProductsList'
+import Pagination from '@material-ui/lab/Pagination'
 
 function SearchPage() {
   const [input, setInput] = useState('')
   const [responseData, setResponseData] = useState([])
   const [error, setError] = useState(false)
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(0)
 
   const fetchData = useCallback(() => {
-    fetch(`http://api.searchspring.net/api/search/search.json?siteId=scmq7n&resultsFormat=native&page=1&q=${input}`)
+    fetch(`http://api.searchspring.net/api/search/search.json?siteId=scmq7n&resultsFormat=native&page=${page}&q=${input}`)
       .then(res => res.json())
       .then(res => {
         console.log(res)
         setResponseData(res.results)
+        setTotalPages(res.pagination.totalPages)
       })
       .catch((err) => {
         setError(true)
         console.log(err)
       })
-  }, [input])
+  }, [input, page])
 
   useEffect(() => {
     fetchData()
@@ -29,10 +33,16 @@ function SearchPage() {
     setInput(input)
   }
 
+  function handlePageChange(event, value) {
+    setPage(value)
+  }
+
   return (
     <div className='container'>
       <SearchBar keyword={input} setKeyword={handleChange} />
+      <Pagination count={totalPages} page={page} onChange={handlePageChange} />
       <ProductsList productsList={responseData} />
+      <Pagination count={totalPages} page={page} onChange={handlePageChange} />
     </div>
   )
 }
